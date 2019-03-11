bb208e5e3c01739e4f77b3150760bd0933c37b45
GomezOrtiz <david.gomez.mail@gmail.com>
2019-03-11 20:12:51 +0100

    All apis more or less working

--- ikeiala/routes/day.js
+++ ikeiala/routes/day.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const router  = express.Router()
 const axios = require('axios')
+const rp = require('request-promise')
+const cheerio = require('cheerio')
+// const igdb = require('igdb-api-node').default;
+
+// const client = igdb('e946f475539429b55eab2775f5d14022');
 
 
 let movies = undefined
 let series = undefined
+let wiki = undefined
 let tomorrow = undefined
 let yesterday = undefined
-
+let titles = []
+let links = []
+let tuits = []
+let meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
 
 router.post('/', (req, res, next) => {
 
@@ -16,11 +25,13 @@ router.post('/', (req, res, next) => {
   const month = parseInt(userDay.slice(5,7)) - 1
   const day = parseInt(userDay.slice(8,10))
 
-
   const date = new Date(year, month, day, 12, 0, 0)
   const dateBefore = new Date(date - 86400000)
   const dateAfter = new Date()
   dateAfter.setTime(date.getTime() + 86400000)
+
+  let today = (parseInt(userDay.slice(2,4)) - 1) + userDay.slice(5,7) + userDay.slice(8)
+  let wikiToday = `${parseInt(userDay.slice(8))}_de_${meses[parseInt(userDay.slice(5,7))]}`
 
   let monthBeforeNormalized = ""
   let monthAfterNormalized = ""
@@ -65,23 +76,104 @@ router.post('/', (req, res, next) => {
     })
 
   let r2 = axios.get(`https://api.themoviedb.org/3/discover/tv?api_key=3aa350912efdcc79b7c8fddde2759632&language=es-ES&sort_by=popularity.desc&air_date.gte=${yesterday}&air_date.lte=${tomorrow}&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false`)
-    .then (answer => {
-        series = answer.data.results
+    .then (response => {
+        series = response.data.results
         return series
     })
     .catch (err => {
         console.log(err)
     })   
 
-  Promise.all([r1,r2])
-    .then (() => {
-      console.log(r1)
-      console.log(r2)
-      res.render('day', {movies, series});
+  let r3 = rp({
+                uri: `https://www.trendinalia.com/twitter-trending-topics/spain/spain-${today}.html`,
+                transform: function (body) {
+                return cheerio.load(body);
+                }
+              })
+          .then($ => {
+          
+              $('a').each(function(i, elem) {
+                      links.push ($(this).attr('href'))
+                      titles.push ($(this).text())
+              })
+              
+              links = links.slice(links.indexOf("https://twitter.com/trendinaliaes") + 1, links.indexOf("https://twitter.com/trendinaliaes") + 11)
+              titles = titles.slice(titles.indexOf("Seguir a @trendinaliaes") + 1, titles.indexOf("Seguir a @trendinaliaes") + 11)
+              
+              tuits = []
+
+              titles.forEach((title, idx) => {
+                tuits.push([title,links[idx]])
+              })
+              
+              return tuits
+          
+          })
+          .catch(function (err) {
+              console.log(err)
+          })
+
+
+  let r4 = axios.get(`https://es.wikipedia.org/w/api.php?action=parse&prop=text&page=Plantilla:Efem%C3%A9rides_-_${wikiToday}&format=json&formatversion=2`)
+  .then (response => {
+      wiki = response.data.parse.text
+      return wiki
+  })
+  .catch (err => {
+      console.log(err)
+  })   
+
+//   let r5 = client.games({
+//     filters: {
+//         'release_dates.date-gt': '2019-03-14',
+//         'release_dates.date-lt': '2019-03-16'
+//     },
+//     limit: 10,
+//     offset: 0,
+//     order: 'release_dates.date:desc',
+//     search: ''
+// }, [
+//     'name',
+//     'release_dates.date',
+//     'summary',
+//     'platforms',
+//     'cover'
+// ])
+// .then (response => {
+//       console.log(response)
+//   })
+//   .catch (err => {
+//       console.log(err)
+//   })   
+
+
+  let r5 = axios({
+    url: "https://api-v3.igdb.com/games",
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'user-key': `e946f475539429b55eab2775f5d14022`
+    },
+    data: `fields name,platforms,storyline,summary,screenshots,release_dates.date; where release_dates.date > ${dateBefore.getTime() / 1000 + 20000} & release_dates.date < ${dateAfter.getTime() / 1000};`
+  })
+    .then(response => {
+      // release_dates.date > 631152000;
+      // {data: `fields name; release_dates.date > ${dateBefore.getTime()} & release_dates.date < ${dateAfter.getTime()}; limit 50`},
+      // data: `fields game,name,platform; date > ${dateBefore.getTime() / 1000} & date < ${dateAfter.getTime() / 1000}`
+        console.log(response.data);
+    })
+    .catch(err => {
+        console.error(err);
+    });
+  
+  Promise.all([r1,r2,r3,r4,r5])
+    .then ((response) => {
+      res.render('day', {movies, series, tuits, wiki})
+      links, titles, tuits = undefined
     })
     .catch (err => {
       console.log(err)
     })
-});
+})
 
 module.exports = router;
