e1b3c51669f286706ae49340ff7bd5102651b2ff
GomezOrtiz <david.gomez.mail@gmail.com>
2019-03-13 11:55:30 +0100

    Small fix for conflicts

--- ikeiala/models/Comment.js
+++ ikeiala/models/Comment.js
@@ -3,7 +3,7 @@ const Schema   = mongoose.Schema;
 
 const commentSchema = new Schema({
   text: String,
-  author: [{type:Schema.Types.ObjectId, ref:"Author"}],
+  author: [{type:Schema.Types.ObjectId, ref:"User"}],
   day: [{type:Schema.Types.ObjectId, ref:"Day"}],
   }, {
   timestamps: true
