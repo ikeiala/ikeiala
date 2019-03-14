27e44d1bbaa4892785c17a92c3c93100a33eab8a
GomezOrtiz <david.gomez.mail@gmail.com>
2019-03-14 09:12:16 +0100

    Cloudinary config working

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -4,12 +4,9 @@ const cloudinaryStorage = require('multer-storage-cloudinary');
 const multer = require('multer');
 
 cloudinary.config({
-  cloud_name: "ikeiala",
-  api_key: 536681684977952,
-  api_secret: "zY-rCtFwMK_k_1MBaWGEe2vEYwY",
-  // cloud_name: process.env.CLOUDINARY_NAME,
-  // api_key: process.env.CLOUDINARY_KEY,
-  // api_secret: process.env.CLOUDINARY_SECRET 
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET 
 });
 
 var storage = cloudinaryStorage({
